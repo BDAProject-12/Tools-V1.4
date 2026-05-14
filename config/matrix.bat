a0cc95054bd284b6894da011a12cf540059019bf
BDAProject-12 <balakosaadhinata@gmail.com>
2026-05-13 21:02:31 -0700

    Update matrix.bat

--- config/matrix.bat
+++ config/matrix.bat
@@ -1,7 +1,13 @@
 @echo off
 title Matrix ^| By : BDA Project
 mode con: cols=45 lines=24
-
-cd ..\..\
 color a
-dir/s
+
+set "hecking=%~dp0hecking.bat"
+start "" %hecking%
+
+:loop
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto loop

204c5868213999608ed283c88e0c5daaec29ded1
BDAProject-12 <balakosaadhinata@gmail.com>
2026-05-13 20:57:46 -0700

    Update matrix.bat

--- config/matrix.bat
+++ config/matrix.bat
@@ -3,17 +3,7 @@ title Matrix ^| By : BDA Project
 mode con: cols=45 lines=24
 start hecking.bat
 
-:matrix
-color 09
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-color 0A
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-color 0C
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
-goto matrix
+cd ..\..\
+color a
+dir/s
+exit
